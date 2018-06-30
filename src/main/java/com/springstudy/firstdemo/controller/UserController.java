846d47b8c8d158685e938c5191b50bb2deff7d22
mskwon25 <canopus.kwon@gmail.com>
2018-07-01 00:16:01 +0900

    Controller 추가

---
+++ src/main/java/com/springstudy/firstdemo/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.springstudy.firstdemo.controller;
+
+import com.springstudy.firstdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
+    @GetMapping("/")
+    public String welcome(Model model) {
+        model.addAttribute("time", new Date());
+        model.addAttribute("message", this.message);
+        return "student";
+    }
+}
